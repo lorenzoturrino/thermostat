ad448621033a8fdf312126adc496d3739a01744f
Lorenzo Turrino <lorenzo.turrino@gmail.com>
2016-03-30 11:31:20 +0100

    implement power saving mode

--- jasmine/spec/ThermostatSpec.js
+++ jasmine/spec/ThermostatSpec.js
@@ -12,10 +12,30 @@ describe('Thermostat', function(){
     });
   });
 
+  describe('#saveMode', function(){
+    it('is on by default', function(){
+      expect(testThermostat.saveMode).toEqual(true);
+    });
+  });
+
   describe('#increaseTemperature', function(){
     it('increases the temp by 1', function(){
       testThermostat.increaseTemperature();
       expect(testThermostat.temp).toEqual(21);
+    });
+    it('cannot increase the temp higher than 25 when powersave is on', function(){
+      testThermostat.temp = 25;
+      testThermostat.saveMode = true;
+      testThermostat.increaseTemperature();
+      expect(testThermostat.temp).toEqual(25);
+    });
+    it('cannot increase the temp higher than 32 when powersave is off', function(){
+      testThermostat.temp = 23;
+      testThermostat.saveMode = false;
+      for(t=0;t<10;t++) {
+        testThermostat.increaseTemperature();
+      };
+      expect(testThermostat.temp).toEqual(32);
     });
   });
 
@@ -26,6 +46,7 @@ describe('Thermostat', function(){
     });
     it('cannot decrease the temp lower than 10', function(){
       testThermostat.temp = 10;
+      testThermostat.decreaseTemperature();
       expect(testThermostat.temp).toEqual(10);
     });
   });
--- jasmine/src/Thermostat.js
+++ jasmine/src/Thermostat.js
@@ -1,9 +1,12 @@
 function Thermostat(){
   this.temp = 20;
+  this.saveMode = true;
 }
 
 Thermostat.prototype.increaseTemperature = function(){
-  this.temp++;
+  if(this.saveMode && this.temp < 24 || !this.saveMode && this.temp < 32){
+    this.temp++;
+  };
 };
 
 Thermostat.prototype.decreaseTemperature = function(){
